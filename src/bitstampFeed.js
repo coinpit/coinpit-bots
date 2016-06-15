f111b375a0c9a881b1c0afcc2248ae9807d84aa7
Bharath Rao <bharath.rao@gmail.com>
2016-06-15 11:33:23 -0700

    detect disconnect

--- src/bitstampFeed.js
+++ src/bitstampFeed.js
@@ -1,15 +1,23 @@
 var bluebird  = require('bluebird')
 module.exports = function(listener){
-  var PusherClient = require('pusher-client')
-  var pusherClient = new PusherClient('de504dc5763aeef9ff52')
-  var channel      = pusherClient.subscribe('live_trades')
-  var orderCh      = pusherClient.subscribe('order_book')
+  var Pusher = require('pusher-client')
+  var pusher = new Pusher('de504dc5763aeef9ff52')
+  pusher.connection.bind('error', function(err) {
+    console.log('PUSHER ERR', err)
+  })
+
+  pusher.connection.bind('disconnected', function(err) {
+    console.log('PUSHER DISCONNECT', err)
+  })
+
+  var channel      = pusher.subscribe('live_trades')
+  // var orderCh      = pusher.subscribe('order_book')
   channel.bind('trade', bluebird.coroutine(function*(data) {
       listener(data.price)
   }))
-  orderCh.bind('data', bluebird.coroutine(function*(data) {
+  // orderCh.bind('data', bluebird.coroutine(function*(data) {
     // console.log('Bitstamp bid', data.bids[0][0])
     // console.log('Bitstamp ask', data.asks[0][0])
-  }))
+  // }))
 
 }
