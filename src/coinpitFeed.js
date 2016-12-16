3ca04ca1a21b1559f66daa17d596f04407a1ce03
Nirmal Gupta <nirmalk.gupta@gmail.com>
2016-12-15 22:34:04 -0800

    fuzz bot.

--- src/coinpitFeed.js
+++ src/coinpitFeed.js
@@ -2,22 +2,26 @@ module.exports = function (listener, socket) {
   // var socket = require("socket.io-client")(coinpitUrl, { rejectUnauthorized: true })
 
   socket.on('trade', function (trade) {
-    process.nextTick(listener.trade.bind(listener, trade.price))
+    if (listener.trade)
+      process.nextTick(listener.trade.bind(listener, trade.price))
   })
 
   socket.on('orderbook', function (orderbook) {
   })
 
   socket.on('priceband', function (band) {
-    process.nextTick(listener.priceband.bind(listener, band))
+    if (listener.priceband)
+      process.nextTick(listener.priceband.bind(listener, band))
   })
 
   socket.on('user_message', function (msg) {
-    process.nextTick(listener.userMessage.bind(listener))
+    if (listener.userMessage)
+      process.nextTick(listener.userMessage.bind(listener))
   })
 
   socket.on('order_patch', function (response) {
-    process.nextTick(listener.orderPatch.bind(listener, response))
+    if (listener.orderPatch)
+      process.nextTick(listener.orderPatch.bind(listener, response))
   })
 
   socket.on('difforderbook', function (difforderbook) {
---
+++ src/fuzzBot.js
@@ -0,0 +1,179 @@
+var bluebird  = require('bluebird')
+var mangler   = require('mangler')
+var affirm    = require('affirm.js')
+var botParams = require('./botParams')(process.argv[2])
+var _         = require('lodash')
+var util      = require('util')
+
+var bot = bluebird.coroutine(function* mmBot(botParams) {
+  var baseurl     = botParams.baseurl
+  var wallet      = botParams.wallet
+  var cc          = require("coinpit-client")(baseurl)
+  var account     = yield cc.getAccount(wallet.privateKey)
+  account.logging = true
+  var bands       = {}
+  var listener    = {}
+
+  listener.priceband = function (priceBand) {
+    bands = priceBand
+  }
+
+  function* init() {
+    require('./coinpitFeed')(listener, account.socket)
+    bands = account.getIndexBands()
+    setInterval(bluebird.coroutine(function*() {
+      if (_.isEmpty(bands)) return
+      yield* randomAction()()
+    }), 100)
+  }
+
+//*********** actions ***********************************************************
+
+  function* create() {
+    var openOrders                     = account.getOpenOrders()
+    var symbol                         = symbolWithMinOrders(openOrders)
+    var orderType                      = randomOrderType()
+    var side                           = randomSide(symbol)
+    var price                          = orderType === 'MKT' ? undefined : randomPrice(symbol, side)
+    var order                          = {
+      clientid   : account.newUUID(),
+      userid     : account.userid,
+      side       : side,
+      quantity   : randomQuantity(),
+      price      : price,
+      orderType  : orderType,
+      stopPrice  : randomStopPoints(),
+      targetPrice: randomTargetPoints(),
+      crossMargin: randomIsCrossMargin(),
+      instrument : symbol
+    }
+    openOrders[symbol][order.clientid] = order
+    var availableMargin                = account.calculateAvailableMarginIfCrossShifted(openOrders)
+    if (availableMargin >= 0) {
+      yield account.patchOrders({ creates: [order] })
+    } else {
+      yield* remove()
+    }
+  }
+
+  function* update() {
+
+  }
+
+  function* remove() {
+    var allOrders = account.getOpenOrders()
+    var symbol    = symbolWithMaxOrders(allOrders)
+    var orders    = allOrders[symbol]
+    var order     = getRandom(_.values(orders))
+    if (order)
+      yield account.patchOrders({ cancels: [order] })
+  }
+
+  function* merge() {
+
+  }
+
+  function* split() {
+
+  }
+
+  function* restCreate() {
+
+  }
+
+  function* restUpdate() {
+
+  }
+
+  function* restDelete() {
+
+  }
+
+//*********** fuzzy ***********************************************************
+
+  var actions = [create, update, remove, merge, split, restCreate, restUpdate, restDelete]
+
+  function randomAction() {
+    return getRandom([create, update])
+  }
+
+  function randomPrice(symbol, side) {
+    var delta = side === 'buy' ? numberBetween(-200, 20) / 100 : numberBetween(-20, 200) / 100
+    return (bands[symbol].price + delta).toFixed(instrument(symbol).ticksize) - 0
+  }
+
+  function randomTargetPoints() {
+    return numberBetween(0, 10)
+  }
+
+  function randomStopPoints() {
+    return numberBetween(2, 10)
+  }
+
+  function randomQuantity() {
+    return numberBetween(1, 5)
+  }
+
+  function randomIsCrossMargin() {
+    return numberBetween(1, 100) % 10 === 0
+  }
+
+  var orderTypes = { 1: "MKT", 2: "STM", 3: "SLM" }
+
+  function randomOrderType() {
+    return orderTypes[numberBetween(1, 20)] || "LMT"
+  }
+
+  function randomSide(symbol) {
+    var orders = account.getOpenOrders()[symbol]
+    if (_.isEmpty(orders)) return numberBetween(1, 20) % 2 === 0 ? 'buy' : 'sell'
+    var sideCount = { buy: 0, sell: 0 }
+    _.values(orders).forEach(order => sideCount[order.side] += toBeFilled(order))
+    return sideCount.buy - sideCount.sell > 0 ? 'sell' : 'buy'
+  }
+
+  function symbolWithMinOrders(orders) {
+    return sortedOrdersPerSymbol(orders)[0].symbol
+  }
+
+  function symbolWithMaxOrders(orders) {
+    var ordersPerSymbol = sortedOrdersPerSymbol(orders)
+    return ordersPerSymbol[ordersPerSymbol.length - 1].symbol
+  }
+
+  function sortedOrdersPerSymbol(orders) {
+    orders              = orders || account.getOpenOrders()
+    var ordersPerSymbol = account.config.instruments.map(symbol => {
+      var count = { symbol: symbol, size: 0 }
+      Object.keys(orders[symbol]).forEach(uuid => count.size += toBeFilled(orders[symbol][uuid]))
+      return count
+    })
+    ordersPerSymbol.sort(function (a, b) {
+      return a.size > b.size
+    })
+    return ordersPerSymbol
+  }
+
+  function numberBetween(start, end) {
+    return Math.round(Math.random() * (end - start)) + start
+  }
+
+  function toBeFilled(order) {
+    return order.quantity - (order.filled || 0) - (order.cancelled || 0)
+  }
+
+  function getRandom(list) {
+    var count = list.length
+    if (count === 0) return undefined
+    var index = Math.round(Math.random() * 1000) % count
+    return list[index]
+  }
+
+  function instrument(symbol) {
+    return account.config.instrument[symbol]
+  }
+
+  yield* init()
+})
+
+bot(botParams)
